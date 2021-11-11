87abce03b7f3021e717ea613155249ba3ae6b13d
Csonka Mihaly <csonka.mihaly@hotmail.com>
2021-11-11 17:59:05 +0100

    Fix syntax after rename

--- src/block/block_answer.rs
+++ src/block/block_answer.rs
@@ -17,8 +17,10 @@ impl TryFrom<&mut BlockQuestion> for BlockAnswer {
     type Error = anyhow::Error;
 
     fn try_from(question: &mut BlockQuestion) -> Result<Self, Self::Error> {
+        // TODO: check if all bytes are locked
+
         let intermediate = question.tweakable_block_solution()
-            ^ &Block::incremental_padding(&question.block_size());
+            ^ &Block::new_incremental_padding(&question.block_size());
         let intermediate = intermediate?;
 
         let plaintext = &intermediate ^ question.original_tweakable_block();
