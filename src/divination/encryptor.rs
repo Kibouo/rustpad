69536ca5d3fd5d59b26af4d3995b20e92961ba6a
Csonka Mihaly <csonka.mihaly@hotmail.com>
2022-01-11 19:09:38 +0100

    Fix caching of decryption-direction block pairs during encryption

--- src/divination/encryptor.rs
+++ src/divination/encryptor.rs
@@ -41,6 +41,7 @@ where
         }
     }
 
+    // encryption looks for the intermediate of the cypher text block, which is then xor-ed with the plain text block to create the cypher text block to be prepended.
     pub fn encrypt_plain_text(
         &self,
         plain_text: &PlainText,
@@ -52,10 +53,10 @@ where
 
         // encrypting requires iteratively, backwards, building up the cypher text. Unlike with decrypting, we don't know each block beforehand. So obviously this can't be done in parallel
         for (i, plain_text_block) in plain_text.blocks().iter().rev().enumerate() {
-            let intermediate = if i == 0 {
+            if i == 0 {
+                // decryption of this block is already finished as it's simply the initial block (the last block of the cypher text)
                 let block_solution = self.initial_block_solution.forged_block_solution();
 
-                // decryption of this block is already finished as it's simply the initial block (the last block of the cypher text)
                 (self.update_ui_callback.clone())(UiEvent::Control(
                     UiControlEvent::ProgressUpdate(*plain_text_block.block_size() as usize),
                 ));
@@ -65,19 +66,30 @@ where
                         plain_text.blocks().len() - i,
                     ),
                 ));
-                block_solution.to_intermediate()
+
+                let prepend_cypher_text_block =
+                    &block_solution.to_intermediate() ^ plain_text_block;
+                encrypted_blocks_backwards.push(prepend_cypher_text_block.clone());
+
+                cache_decryption_equivalent(
+                    cache.clone(),
+                    prepend_cypher_text_block,
+                    self.initial_block_solution.block_to_decrypt().clone(),
+                    block_solution.clone(),
+                )?;
             } else {
+                // Use only the last block, as forging a block is only dependant on its "pair". Not having to send all blocks saves bandwidth and processing time, for us and the oracle
+                let cypher_text_block = encrypted_blocks_backwards
+                    .last()
+                    .expect("Encryption starts with 1 block, yet no block was found in the list")
+                    .clone();
+
                 // prepend an empty block which is to serve as the forgeable block
                 let blocks_to_solve = vec![
                     Block::new(&plain_text_block.block_size()),
-                    // Use only the last block, as forging a block is only dependant on its "pair". Not having to send all blocks saves bandwidth and processing time, for us and the oracle
-                    encrypted_blocks_backwards
-                        .last()
-                        .expect(
-                            "Encryption starts with 1 block, yet no block was found in the list",
-                        )
-                        .clone(),
+                    cypher_text_block.clone(),
                 ];
+
                 let forged_cypher_text = ForgedCypherText::from_slice(
                     &blocks_to_solve[..],
                     plain_text_block.block_size(),
@@ -108,7 +120,18 @@ where
                         plain_text.blocks().len() - i,
                     ),
                 ));
-                block_solution.to_intermediate()
+
+                // if this is the last block, it's the IV
+                let prepend_cypher_text_block =
+                    &block_solution.to_intermediate() ^ plain_text_block;
+                encrypted_blocks_backwards.push(prepend_cypher_text_block.clone());
+
+                cache_decryption_equivalent(
+                    cache.clone(),
+                    prepend_cypher_text_block,
+                    cypher_text_block,
+                    block_solution.clone(),
+                )?;
             };
 
             info!(
@@ -117,9 +140,6 @@ where
                 i + 1,
                 plain_text.amount_blocks()
             );
-            // if this is the last block, it's the IV
-            let new_cypher_text_block = &intermediate ^ plain_text_block;
-            encrypted_blocks_backwards.push(new_cypher_text_block);
         }
 
         Ok(CypherText::from_iter(
@@ -128,4 +148,25 @@ where
             *self.initial_block_solution.used_encoding(),
         ))
     }
+}
+
+// encryption uses a (dummy block, cypher block)-pair to build the actual cypher text block to prepend. `solve_block` will cache this pair, instead of the eventual (cypher block - 1, cypher block)-pair. We store this 2nd type of pair here.
+fn cache_decryption_equivalent(
+    cache: Arc<Mutex<Option<Cache>>>,
+    prepend_cypher_text_block: Block,
+    cypher_text_block: Block,
+    block_solution: Block,
+) -> Result<()> {
+    cache
+        .lock()
+        .unwrap()
+        .as_mut()
+        .map(|cache| {
+            cache.insert(
+                (prepend_cypher_text_block, cypher_text_block),
+                block_solution,
+            )
+        })
+        .transpose()
+        .map(|_| ())
 }
